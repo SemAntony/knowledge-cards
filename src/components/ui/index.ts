1db7da74a0739d9793821081501a60fa30c3e26c
Sem Antony <sem.antony.dev@gmail.com>
2024-12-14 22:18:35 +0400

    add index for all components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,43 @@
+export * from './avatar'
+
+export * from './button'
+
+export * from './card'
+
+export * from './checkbox'
+
+export * from './dialog'
+
+export * from './dropdown'
+
+export * from './dynamicTable'
+
+export * from './form-controller'
+
+export * from './label'
+
+export * from './loader'
+
+export * from './modal'
+
+export * from './page'
+
+export * from './pageSizeSelect'
+
+export * from './pagination'
+
+export * from './radioGroup'
+
+export * from './rating'
+
+export * from './select'
+
+export * from './slider'
+
+export * from './tables'
+
+export * from './tabs'
+
+export * from './textField'
+
+export * from './typography'
